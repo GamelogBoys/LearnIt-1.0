37218cbabeefa3dcb464bd46323f89de07e57315
GamelogBoys <gamelogboys@gmail.com>
2026-07-16 15:33:21 +0530

    removed non-functional files

--- netlify/functions/app.js
+++
@@ -1,5 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../../app'); // 👈 This imports your main app.js
-
-// Wrap the Express app inside serverless-http
-module.exports.handler = serverless(app);
